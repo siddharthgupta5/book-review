d0a64077a7aa24d5ff13022ad3e58649c35fa0c5
SIDDHARTHGUPTA <contactsid14@gmail.com>
2025-05-30 16:30:46 +0530

    server entry point

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const app = require('./app');
+const connectDB = require('./config/db');
+const http = require('http');
+
+// Load env vars
+require('dotenv').config();
+
+// Connect to database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
